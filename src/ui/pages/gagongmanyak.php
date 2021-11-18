822688502d84fa2de642fff3b866cc921260fd0c
ScriptycaLiSA <jmontizon143@gmail.com>
2021-11-18 20:54:48 +0800

    Gagong manyak by Mikko

---
+++ src/ui/pages/gagongmanyak.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+  <title>Gagong | POS System</title>
+  <?php include 'components/head_content.php' ?>
+  <link rel = "stylesheet" href = "http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <style>
+      i.fields {font-size: 2em; color: gray;}
+  </style>
+</head>
+<body>
+</body>
