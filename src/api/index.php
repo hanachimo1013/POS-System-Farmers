a3e220af44b35d1d52922cccd4f434fb99d2fd38
Jake Montizon <75973651+ScriptycaLiSA@users.noreply.github.com>
2021-12-17 18:07:13 +0800

    Mayaten boin Romar, fetch mo lang atuyen

--- src/api/index.php
+++ src/api/index.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-require '../../../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 $app = new \Slim\App();
 
